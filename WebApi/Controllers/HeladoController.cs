1cab52d30ce872cd1b444103b07925ba8c8548bf
eduardovr88 <133454595+eduardovr88@users.noreply.github.com>
2023-11-19 20:29:02 -0600

    Update HeladoController.cs

--- WebApi/Controllers/HeladoController.cs
+++ WebApi/Controllers/HeladoController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Http;
-using WebApi.Data; // Asegúrate de tener una clase HeladoData que interactúe con la base de datos.
+using WebApi.Data; 
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -11,33 +11,33 @@ namespace WebApi.Controllers
         // GET api/helado
         public List<Helado> Get()
         {
-            return HeladoData.Listar(); // Llama al procedimiento almacenado para listar helados
+            return HeladoData.Listar();
         }
 
         // GET api/helado/5
         public Helado Get(int id)
         {
-            return HeladoData.Obtener(id); // Llama al procedimiento almacenado para obtener un helado por ID
+            return HeladoData.Obtener(id); 
         }
 
         // POST api/helado
         public bool Post([FromBody] Helado oHelado)
         {
-            // Llama al procedimiento almacenado para registrar un helado
+            
             return HeladoData.Registrar(oHelado.Sabor, oHelado.Tamano, oHelado.Precio);
         }
 
         // PUT api/helado/5
         public bool Put(int id, [FromBody] Helado oHelado)
         {
-            // Llama al procedimiento almacenado para modificar un helado
+    
             return HeladoData.Modificar(id, oHelado.Sabor, oHelado.Tamano, oHelado.Precio);
         }
 
         // DELETE api/helado/5
         public bool Delete(int id)
         {
-            // Llama al procedimiento almacenado para eliminar un helado por ID
+           
             return HeladoData.Eliminar(id);
         }
     }
